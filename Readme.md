35e20f895930507aa7b8e9bd785f5bad2d745add
NickolasBenakis <nickolasbenele@gmail.com>
2019-05-18 19:29:50 +0300

    init commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Javascript Theory
+
+## Part I : Memory Heap, Event loop, Call Stack, callback queue, Web Apis
+    I created a snippet for making a playground around call Stack 
---
+++ snippet.js
@@ -0,0 +1,22 @@
+
+// call Stack
+console.log("1");
+setTimeout(()=>{ console.log("2")},2000);
+console.log("3");
+console.log("4");
+
+
+
+// Process 
+
+// Call Stack
+
+
+// Web Api
+
+
+// Callback Queue
+
+
+// Event Loop
+
